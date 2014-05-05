fd508dcc5a32af948026dd5e2ba176e02f80c8ec
anilmaurya <anilmaurya8dec@gmail.com>
2014-05-05 18:26:19 +0530

    initial commit

---
+++ src/scenes.js
@@ -0,0 +1,16 @@
+Crafty.scene('Game', function(){
+    Crafty.e('Player').at(5,5);
+});
+
+Crafty.scene('Loading', function(){
+    Crafty.e('2D, DOM, Text')
+        .text('Loading....');
+    Crafty.load(['assets/monkeysprites.png'], function(){
+        Crafty.sprite(68, 100, 'assets/monkeysprites.png', {
+            happy_monkey: [0, 0],
+            ready_monkey: [4, 0]
+        });
+        Crafty.scene('Game');
+    })
+});
+
