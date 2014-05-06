50df78b16476aebc20f631ba26d415b05b193613
Sanjiv Jha <narutosanjiv@gmail.com>
2014-05-06 13:28:19 +0530

    convert to node app

---
+++ public/javascripts/game.js
@@ -0,0 +1,25 @@
+Game = {
+
+    map_grid: {
+        width: 40,
+        height: 30,
+        tile: {
+            width: 16,
+            height: 16
+        }
+    },
+
+    width: function(){
+        return this.map_grid.width * this.map_grid.tile.width;
+    },
+
+    height: function(){
+        return this.map_grid.height * this.map_grid.tile.height;
+    },
+
+    start: function(){
+        Crafty.init(Game.width(), Game.height());
+        Crafty.background('blue');
+        Crafty.scene('Loading');
+    }
+}
---
+++ public/javascripts/scenes.js
@@ -0,0 +1,16 @@
+Crafty.scene('Game', function(){
+    Crafty.e('Player').at(5,5);
+});
+
+Crafty.scene('Loading', function(){
+    Crafty.e('2D, DOM, Text')
+        .text('Loading....');
+    Crafty.load(['/images/monkeysprites.png'], function(){
+        Crafty.sprite(68, 80, '/images/monkeysprites.png', {
+            happy_monkey: [0, 0],
+            ready_monkey: [4, 0]
+        });
+        Crafty.scene('Game');
+    })
+});
+
