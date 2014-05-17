7616c43bb717e8dbf1361d584144e0d67bb1098f
anilmaurya <anilmaurya8dec@gmail.com>
2014-05-18 03:33:56 +0530

    adding wall and bullet fire

--- public/javascripts/components.js
+++ public/javascripts/components.js
@@ -21,6 +21,14 @@ Crafty.c('Actor', {
     },
 });
 
+Crafty.c('Wall', {
+    init: function(){
+        this.requires('Actor, Solid, wall')
+    },
+})
+/*
+   To move player in multi direction
+
 Crafty.c('Player', {
     init: function(){
         this.requires('Actor, Multiway, ready_monkey, Collision, SpriteAnimation')
@@ -37,7 +45,7 @@ Crafty.c('Player', {
         } else if (data.x < 0){
             this.animate('PlayerMovingLeft', animation_speed, -1); 
         } else {
-            this.animate('stop', animation_speed, -1); 
+            //this.animate('stop', animation_speed, -1); 
         }
     
     })
@@ -55,8 +63,69 @@ Crafty.c('Player', {
     }
 });
 
-Crafty.c('Player1', {
+*/
+
+Crafty.c('Player', {
     init: function(){
-        this.requires('Actor, ready_monkey')
+        this.requires('Actor, ready_monkey, Gravity, Twoway, Animate, Collision, SpriteAnimation')
+        .reel('PlayerMovingLeft', 500, 1, 1, 3)
+        .reel('PlayerMovingRight', 500, 3, 0, 3)
+        .reel('PlayerRight', 5, 3, 0, 1)
+        .reel('PlayerLeft', 5, 1, 1, 1)
+        .attr({facingRight: true})
+        .stopOnSolids();
+    var animation_speed = 8;
+    this.bind('NewDirection', function(data){
+        if(data.x > 0) {
+            this.facingRight = true
+            this.animate('PlayerMovingRight', animation_speed, -1);
+        } else if (data.x < 0){
+            this.facingRight = false
+            this.animate('PlayerMovingLeft', animation_speed, -1); 
+        } else {
+            if(this.facingRight){
+                this.animate('PlayerRight', animation_speed, -1); 
+            }
+            else{
+                this.animate('PlayerLeft', animation_speed, -1); 
+            }
+            //this.animate('stop', animation_speed, -1); 
+        }
+    })
+    },
+    stopOnSolids: function(){
+        this.onHit('Solid', this.stopMovement);
+        return this;
+    },
+    stopMovement: function(){
+        this._speed = 0;
+        if (this._movement){
+            this.x -= this._movement.x;
+            this.y -= this._movement.y;
+        }
+    }
+});
+
+//create the bullet component 
+Crafty.c("bullet", {
+    init: function(){
+        this.requires('2D, DOM, Color, Collision')
+        .destroyOnHit();
+    },
+    destroyOnHit: function(){
+        this.onHit('Solid', this.destroyBullet);
+        return this;
+    },
+    destroyBullet: function(){
+        this.destroy();
+        return this;
+    },
+    bullet: function(dir) {
+        this.bind("EnterFrame", function() {
+            this.move(dir, 15);
+            if(this.x > Crafty.viewport.width || this.x < 0) 
+            this.destroy();
+        });
+        return this;
     }
 });
--- public/javascripts/scenes.js
+++ public/javascripts/scenes.js
@@ -1,17 +1,56 @@
 Crafty.scene('Game', function(){
     //Crafty.e('Player').at(5,5);
-    var player = Crafty.e('2D, object, Player, Tweener').attr({
-        x: 10,
-        y: 0
-    }).addTween({ 
-        y: 200
-    }, 'easeOutBounce', 200);
+    /*var mouseTracker = Crafty.e("2D, Mouse").attr({w: Game.width(), h: Game.height(), x: 0, y: 0});
+    mouseTracker.bind('MouseMove', function(e){
+        player.x = Crafty.mousePos.x;
+        player.y = Crafty.mousePos.y;
+    });
+    */
+    var stair_length = 5;
+    var stair_height = (1/3)* Game.map_grid.height;
+    for (var x = 0; x < Game.map_grid.width; x++) {
+        var increment_stair_length = false;
+        for (var y = 0; y < Game.map_grid.height; y++){
+            var at_edge = x == 0 || x > Game.map_grid.width - 3  || y == 0 || y > Game.map_grid.height - 3;
+            if (at_edge) {
+                Crafty.e('Wall').at(x,y);
+            }
+            if (y > stair_height && x <= stair_length){
+                Crafty.e('Wall').at(x,y);
+                increment_stair_length = true;
+            }
+        }
+        if(increment_stair_length && x >= stair_length){
+            stair_length +=5 ;
+            stair_height +=3 ;
+        }
+    }
+
+    var player = Crafty.e('2D, object, Player, DOM, collision, twoway').attr({
+        x: 50,
+        y: 50
+    }).gravity('Wall').twoway(7).gravityConst(1);
+    player.bind('KeyDown', function(e){
+        if(e.key == Crafty.keys.SPACE){
+            bullet_x = this.x + 32;
+            bullet_y = this.y + 30;
+            if(this.facingRight){
+                bullet_dir = 'e';
+            }
+            else{
+                bullet_dir = 'w';
+            }
+            Crafty.e('bullet').attr({x: bullet_x, y: (this.y + 30), w: 10, h: 5, z: 50}).color("rgb(250,0,0)").bullet(bullet_dir);
+        }
+    })
+
 });
 
 Crafty.scene('Loading', function(){
     Crafty.e('2D, DOM, Text')
         .text('Loading....');
-    Crafty.load(['/images/transparent.png'], function(){
+    Crafty.load(['/images/wall.jpg', '/images/transparent.png'], function(){
+        Crafty.sprite(50, 50, '/images/wall.jpg', {wall: [1,0]});
         Crafty.sprite(68, 80, '/images/transparent.png', {
             happy_monkey: [0, 0],
             ready_monkey: [4, 0]
